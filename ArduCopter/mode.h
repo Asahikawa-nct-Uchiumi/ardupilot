85e2e23c8d622ee2e8567157f3203d94cea0d6f6
Asahikawa-nct-Uchiumi <p258003@edu.asahikawa-nct.ac.jp>
2025-10-02 16:17:59 +0900

    二か所追加

--- ArduCopter/mode.h
+++ ArduCopter/mode.h
@@ -1670,6 +1670,33 @@ private:
 
 };
 
+class ModeArmTip : public Mode {
+
+public:
+    // inherit constructor
+    using Mode::Mode;
+    Number mode_number() const override { return Number::ARM_TIP; }
+
+    virtual void run() override;
+
+    bool requires_GPS() const override { return false; }
+    bool has_manual_throttle() const override { return true; }
+    bool allows_arming(AP_Arming::Method method) const override { return true; };
+    bool is_autopilot() const override { return false; }
+    bool allows_save_trim() const override { return true; }
+    bool allows_auto_trim() const override { return true; }
+    bool allows_autotune() const override { return true; }
+    bool allows_flip() const override { return true; }
+
+protected:
+
+    const char *name() const override { return "ARM_TIP"; }
+    const char *name4() const override { return "AMTP"; }
+
+private:
+
+};
+
 #if FRAME_CONFIG == HELI_FRAME
 class ModeStabilize_Heli : public ModeStabilize {
 
